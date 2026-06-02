505e06e63fe7c99a167ea943f2b7b1daf0834bea
qinglin_zcr <dzmttdm@163.com>
2026-06-02 22:39:07 +0800

    Initial commit

---
+++ src/main/java/com/qinglin/just_enough_lightmans_trades/JustEnoughLightmansTrades.java
@@ -0,0 +1,29 @@
+package com.qinglin.just_enough_lightmans_trades;
+
+import com.mojang.logging.LogUtils;
+import net.minecraft.client.Minecraft;
+import net.minecraft.world.item.CreativeModeTabs;
+import net.minecraft.world.level.block.Blocks;
+import net.minecraftforge.api.distmarker.Dist;
+import net.minecraftforge.common.MinecraftForge;
+import net.minecraftforge.event.BuildCreativeModeTabContentsEvent;
+import net.minecraftforge.event.server.ServerStartingEvent;
+import net.minecraftforge.fml.common.Mod;
+import net.minecraftforge.fml.config.ModConfig;
+import net.minecraftforge.fml.event.lifecycle.FMLClientSetupEvent;
+import net.minecraftforge.fml.event.lifecycle.FMLCommonSetupEvent;
+import net.minecraftforge.fml.javafmlmod.FMLJavaModLoadingContext;
+import net.minecraftforge.registries.ForgeRegistries;
+import org.slf4j.Logger;
+
+// The value here should match an entry in the META-INF/mods.toml file
+@Mod(JustEnoughLightmansTrades.MOD_ID)
+public class JustEnoughLightmansTrades
+{
+    // Define mod id in a common place for everything to reference
+    public static final String MOD_ID = "just_enough_lightmans_trades";
+
+
+
+
+}
